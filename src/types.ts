900fec597c8cc5149927ad5a1763f1864e3f1592
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-14 22:40:56 +0530

    -- add prettierrc -- updated types

--- src/types.ts
+++ src/types.ts
@@ -5,19 +5,19 @@ export interface Karban {
   projects: KarbanProjects[];
 }
 
-export interface KarbanProjects {
+export type KarbanProjects = {
   projectId: string;
   projectName: string;
   projectDescription: string;
   tabs: KarbanProjectTab[];
-}
-export interface KarbanProjectTab {
+};
+export type KarbanProjectTab = {
   tabId: string;
   tabName: string;
   cards: KarbanProjectTabCard[];
-}
+};
 
-export interface KarbanProjectTabCard {
+export type KarbanProjectTabCard = {
   cardId: string;
   cardBody: string;
-}
+};
