d70c18e8b76d4f9efcf730fbfe9c360996b5e642
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-14 15:23:21 +0530

    add Modal Component

---
+++ src/Modal.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+ export let showModal: boolean = false;
+</script>
+
+<style>
+ .backdrop {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.8);
+ }
+ .modal {
+  padding: 50px;
+  border-radius: 10px;
+  max-width: 500px;
+  margin: 10% auto;
+  text-align: center;
+  background: white;
+ }
+</style>
+
+{#if showModal}
+ <div class="backdrop" on:click|self>
+  <div class="modal">
+   <slot />
+  </div>
+ </div>
+{/if}
