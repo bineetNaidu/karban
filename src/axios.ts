2c811e21715f650c7a824309e56a9860f6b03743
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-15 13:07:29 +0530

    migrated to sapper

--- src/axios.ts
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios';
-
-const axios = Axios.create({
-  baseURL: 'https://api-bineetnaidu-io.herokuapp.com/api/v1/karban',
-  responseType: 'json',
-});
-
-export default axios;
--- src/store/KarbanStore.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-import type { Karban } from '../types';
-
-//? mock overlook
-let data: Karban | undefined = undefined;
-
-const KarbanStore = writable(data);
-
-export default KarbanStore;
