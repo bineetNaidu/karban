129daf23f9b0088749f17d9d6026d8c2239ed2ae
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-15 23:35:00 +0530

    add sapper setup for others

--- src/store/KarbanStore.ts
+++ src/store/KarbanStore.ts
@@ -1,9 +1,16 @@
 import { writable } from 'svelte/store';
 import type { Karban } from '../types';
+import axios from '../utils/axios';
 
 //? mock overlook
 let data: Karban | undefined = undefined;
 
 const KarbanStore = writable(data);
+
+(async () => {
+  const res = await axios.get('/5fd8f1885831be0015b2f143');
+  data = res.data.karban;
+  KarbanStore.update(() => data);
+})();
 
 export default KarbanStore;
