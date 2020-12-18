53854daf857638c7e2fa573009efc3427ee0aa31
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-18 22:45:12 +0530

    add base TabCard svelte component

---
+++ src/components/TabCard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+ import type { KarbanProjectTab } from "../types";
+
+ export let tab: KarbanProjectTab;
+</script>
+
+<div class="bg-gray-100 rounded-xl p-8 w-64 min-h-screen h-full">
+ <h1>{tab.tabName}</h1>
+</div>
