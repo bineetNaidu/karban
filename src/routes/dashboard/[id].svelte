3ad9febf31c4b8fc6984a3faeb9c02165ec667e1
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-18 21:42:09 +0530

    add base [id].svelte

--- src/routes/dashboard/[id].svelte
+++ src/routes/dashboard/[id].svelte
@@ -1,1 +1,26 @@
-<p>project</p>
+<script context="module">
+ export async function preload(page, session) {
+  const { id } = page.params;
+  return { id };
+ }
+</script>
+
+<script lang="ts">
+ import { onMount } from "svelte";
+ import { goto } from "@sapper/app";
+ import KarbanStore from "../../store/KarbanStore";
+ import type { KarbanProjects } from "../../types";
+
+ export let id;
+
+ let data: KarbanProjects;
+ onMount(async () => {
+  if (!$KarbanStore) {
+   goto("/");
+  }
+
+  data = $KarbanStore.projects.find((p) => p.projectId === id);
+ });
+</script>
+
+<p>Projects</p>
