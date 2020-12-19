f15f3d68ec8368e729accf184329fa87d65457ac
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-19 22:40:33 +0530

    add base CardTabSurface svelte component

---
+++ src/components/CardTabSurface.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+ import type { KarbanProjectTabCard } from "../types";
+ export let card: KarbanProjectTabCard;
+</script>
+
+<div class="bg-gray-100 rounded-xl p-6 mt-4">
+ <h1>{card.cardBody}</h1>
+</div>
