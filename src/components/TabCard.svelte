d3962d3e5ed887386c966fb0c96d287532975483
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-19 21:15:33 +0530

    updated Styles

--- src/components/TabCard.svelte
+++ src/components/TabCard.svelte
@@ -4,6 +4,6 @@
  export let tab: KarbanProjectTab;
 </script>
 
-<div class="bg-gray-100 rounded-xl p-8 w-64 min-h-screen h-full">
+<div class="bg-gray-200 rounded-xl p-6 w-72 min-h-screen h-full">
  <h1>{tab.tabName}</h1>
 </div>
