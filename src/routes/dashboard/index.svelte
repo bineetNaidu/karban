5fc83c44d06aff7841ec965b7a3266e93308c2f9
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-21 20:17:00 +0530

    add base loader in dashboard page

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
- import KarbanStore from "../../store/KarbanStore";
- import Navbar from "../../components/Navbar.svelte";
- import KarbanProjects from "../../components/KarbanProjects.svelte";
+  import { onMount } from 'svelte';
+  import { goto } from '@sapper/app';
+  import KarbanStore from '../../store/KarbanStore';
+  import Navbar from '../../components/Navbar.svelte';
+  import KarbanProjects from '../../components/KarbanProjects.svelte';
+
+  let loading: boolean = true;
+
+  $: projects = undefined;
+  $: username = undefined;
+
+  onMount(() => {
+    KarbanStore.subscribe((data) => {
+      if (!data) goto('/');
+      projects = data.projects;
+      username = data.username;
+      loading = false;
+    });
+  });
 </script>
 
-<main class="">
- <Navbar username={$KarbanStore.username} />
- <KarbanProjects projects={$KarbanStore.projects} />
-</main>
+{#if !loading}
+  <main class="">
+    <Navbar {username} />
+    <KarbanProjects {projects} />
+  </main>
+{:else}
+  <h1>Loading...</h1>
+{/if}
