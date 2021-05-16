80ec7b2727e3814ea2e8b0936f3870490c7c54bc
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-16 22:05:21 +0530

    🚀 add boilerplate apollo server

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,53 +1,54 @@
 // ***** IMPORT *****
-import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
 import connectDB from './config/db';
 import dotenv from 'dotenv';
-import NotFoundError from './utils/NotFoundError';
-import ExpressErrorHandler from './utils/ExpressErrorHandler';
-import { graphqlHTTP } from 'express-graphql';
-import 'express-async-errors';
+import { ApolloServer, gql } from 'apollo-server';
 
 // ***** App Config *****
 dotenv.config();
 
-const app = express();
-connectDB();
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-// ***** Middlewares *****
-app.use(cors());
-helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: [],
-    connectSrc: ["'self'"],
-    scriptSrc: ["'unsafe-inline'", "'self'"],
-    styleSrc: ["'self'", "'unsafe-inline'"],
-    workerSrc: ["'self'", 'blob:'],
-    objectSrc: [],
-    imgSrc: ["'self'", 'blob:', 'data:'],
-    fontSrc: ["'self'"],
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
   },
-});
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    graphiql: true,
-    pretty: true,
-  })
-);
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
 
-//! Not found page error
-app.all('*', () => {
-  throw new NotFoundError();
-});
-// ! Error Handlers
-app.use(ExpressErrorHandler);
+(async () => {
+  await connectDB();
+  const server = new ApolloServer({ typeDefs, resolvers, cors: true });
 
-// **** Listeners ****
-app.listen(process.env.PORT || 4242, () => {
-  console.log('-----------------------------------------');
-  console.log('>>>>>>> KARBAN SERVER HAS STARTED <<<<<<<<');
-  console.log('-----------------------------------------');
-});
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+})();
