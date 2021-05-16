a394d31a9ee010460cc74528c35eba06f031da30
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-17 00:22:23 +0530

    ❇️ add base resolvers and update typeDefs

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,54 +1,24 @@
 // ***** IMPORT *****
 import connectDB from './config/db';
 import dotenv from 'dotenv';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './graphql/typeDefs';
+import { resolvers } from './graphql/resolvers';
 
 // ***** App Config *****
 dotenv.config();
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
 (async () => {
   await connectDB();
-  const server = new ApolloServer({ typeDefs, resolvers, cors: true });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    cors: true,
+    tracing: true,
+  });
 
   // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
+  server.listen({ port: process.env.PORT }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
   });
 })();
--- packages/server/src/graphql/typeDefs.ts
+++ packages/server/src/graphql/typeDefs.ts
@@ -1,45 +1,43 @@
 import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
+  type Card {
+    cardId: String!
+    cardBody: String
+  }
 
-type Card {
-  cardId: String!
-  cardBody: String
-}
+  type Tab {
+    _id: ID
+    tabName: String!
+    cards: [Card]
+  }
 
-type Tab {
-  _id: ID
-  tabName: String!
-  cards: Card[]
-}
+  type Project {
+    _id: ID
+    projectName: String!
+    projectDescription: String!
+    tabs: [Tab]
+  }
 
-type Project {
-  _id: ID
-  projectName: String!
-  projectDescription: String!
-  tabs: Tab[]
-}
+  type User {
+    _id: ID
+    username: String!
+    avatar: String
+    email: String!
+    password: String!
+    projects: [Project]
+  }
 
-type User {
-  _id: ID
-  username: String!
-  avatar: String
-  email: String!
-  password: String!
-  projects: Project[]
-}
+  type Query {
+    allProjects: [Project]
+    allUsers: [Project]
+    getProjectById(id: ID!): Project
+    getTabById(id: ID!): Tab
+  }
 
-type Query {
-  allProjects: [Project]
-  allUsers: [Project]
-  getProjectById(id: ID): Project
-  getTabById(id: ID): Tab
-}
-
-type Mutation {
-  createProject(projectName: String!, projectDescription: String!): Project
-  createTab(projectId: ID!, tabName: String!): Tab
-  createCard(projectId: ID!, cardId: ID!,cardBody: String!): Card
-}
-
+  type Mutation {
+    createProject(projectName: String!, projectDescription: String!): Project
+    createTab(projectId: ID!, tabName: String!): Tab
+    createCard(projectId: ID!, cardId: ID!, cardBody: String!): Card
+  }
 `;
