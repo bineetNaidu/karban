8f0f7f5e6583006c027a2fe4cbbbd61b31107ce4
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-16 11:40:00 +0530

    🎉 add logics to create/new project

--- packages/client/pages/new.tsx
+++ packages/client/pages/new.tsx
@@ -1,8 +1,39 @@
+import React from 'react';
+import router from 'next/router';
+import { useMutation } from '@apollo/client';
+import { useStateValue } from '../data/StateContext';
 import useForm from '../hooks/useForm';
+import { CREATE_PROJECT } from '../utils/queries/createProject';
 
 const NewProjectPage = () => {
   const [projectName, handleProjectname] = useForm('');
   const [projectDescription, handleProjectDesc] = useForm('');
+  const [createProject] = useMutation(CREATE_PROJECT);
+  const [{ user }, dispatch] = useStateValue();
+
+  const handleCreate = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (projectDescription && projectName) {
+      const { data, errors } = await createProject({
+        variables: { projectName, projectDescription, token: user.token },
+      });
+
+      if (errors) return;
+
+      dispatch({
+        type: 'ADD_PROJECT',
+        payload: {
+          _id: data.createProject._id,
+          projectDescription: data.createProject.projectDescription,
+          projectName: data.createProject.projectName,
+          tabs: data.createProject.tabs,
+        },
+      });
+
+      router.push('/dashboard');
+    }
+  };
 
   return (
     <div className="bg-white shadow overflow-hidden sm:rounded-lg m-16">
@@ -14,13 +45,14 @@ const NewProjectPage = () => {
           details and application.
         </p>
       </div>
-      <form className="border-t  border-gray-200">
+      <form className="border-t border-gray-200" onSubmit={handleCreate}>
         <dl>
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">Project name</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
               <input
                 type="text"
+                className="mt-1 ring-indigo-500 border-indigo-500 block w-full shadow-sm sm:text-sm rounded-md"
                 value={projectName}
                 onChange={handleProjectname}
               />
@@ -33,6 +65,7 @@ const NewProjectPage = () => {
             </dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
               <textarea
+                className="shadow-sm ring-indigo-500 border-indigo-500 mt-1 block w-full sm:text-sm rounded-md"
                 value={projectDescription}
                 onChange={handleProjectDesc}
               ></textarea>
