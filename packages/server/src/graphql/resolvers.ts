1e1f75b2423a6123b3b54bc7bcbc321f3d97d887
bineetNaidu <66471461+bineetNaidu@users.noreply.github.com>
2021-05-20 12:07:43 +0530

    Revert "Apollo gql server"

--- packages/server/src/graphql/resolvers.ts
+++
@@ -1,7 +0,0 @@
-import { Query } from './utils/Query';
-import { Mutation } from './utils/Mutation';
-
-export const resolvers: any = {
-  Query,
-  Mutation,
-};
--- packages/server/src/graphql/utils/Query/allUsers.ts
+++
@@ -1,7 +0,0 @@
-import User, { UserDoc } from '../../../models/User';
-
-// @ts-ignore
-export const allUsers: Promise<UserDoc[]> = async () => {
-  const users = await User.find({}).populate('projects').exec();
-  return users;
-};
