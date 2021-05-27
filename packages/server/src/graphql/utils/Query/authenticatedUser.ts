5787fcdcacbe211e1dee906af38bd5921ea89fb8
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-27 23:03:26 +0530

    🎉 better query response

--- packages/server/src/graphql/utils/Query/authenticatedUser.ts
+++ packages/server/src/graphql/utils/Query/authenticatedUser.ts
@@ -2,18 +2,18 @@ import User, { UserDoc } from '../../../models/User';
 import { ContextType } from '../../../utils/createContext';
 
 // @ts-ignore
-export const authenticatedUser: Promise<UserDoc> = async (
+export const authenticatedUser: Promise<UserDoc | null> = async (
   _parent: any,
   _args: any,
   ctx: ContextType
 ) => {
-  if (!ctx.hasAuth) {
-    throw new Error('No Authenticated user');
-  }
-
   const authUser = await User.findOne({ _id: ctx.uid })
     .populate('projects')
     .exec();
+
+  if (!authUser) {
+    return null;
+  }
 
   return authUser;
 };
