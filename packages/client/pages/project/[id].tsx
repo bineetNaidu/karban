8b5f35b365d147035f38a4c7ad55b1da635ea148
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-31 23:43:15 +0530

    ❇️ intregate ui w/ zustand

--- packages/client/pages/project/[id].tsx
+++ packages/client/pages/project/[id].tsx
@@ -6,6 +6,8 @@ import {
   useGetProjectByIdQuery,
 } from '../../generated/graphql';
 import { withApollo } from '../../lib/withApollo';
+import { useProjectStore } from '../../lib/project.store';
+import { useEffect } from 'react';
 
 const Project = () => {
   const router = useRouter();
@@ -14,16 +16,31 @@ const Project = () => {
     variables: { id: router.query.id as string },
   });
 
+  const { setProject, projectName, tabs, reset } = useProjectStore();
+
   const [deleteProject] = useDeleteProjectMutation();
 
   if (loading) return <Spinner />;
 
   if (!data && !loading) return <h1>Somthing went wrong</h1>;
 
+  useEffect(() => {
+    const unsub = setProject({
+      __typename: 'Project',
+      _id: data.getProjectById._id,
+      projectName: data.getProjectById.projectName,
+      projectDescription: data.getProjectById.projectDescription,
+      tabs: data.getProjectById.tabs,
+    });
+    return () => {
+      unsub;
+    };
+  }, []);
+
   const handleDeleteProject = async () => {
     const id = data.getProjectById._id;
     await deleteProject({ variables: { id } });
-
+    reset();
     router.push('/dashboard');
   };
 
@@ -33,9 +50,7 @@ const Project = () => {
 
       <div className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4 w-4/6 m-auto">
         <header className="flex">
-          <h1 className="text-4xl font-bold uppercase flex-1">
-            {data.getProjectById.projectName}
-          </h1>
+          <h1 className="text-4xl font-bold uppercase flex-1">{projectName}</h1>
 
           <div>
             <button className="py-2 px-4 border-black border-1 bg-green-600 rounded mx-2 text-white">
@@ -56,7 +71,7 @@ const Project = () => {
         <hr />
 
         <section className="flex">
-          {data.getProjectById.tabs.map((p) => (
+          {tabs.map((p) => (
             <div key={p._id}>
               <h1>{p.tabName}</h1>
               <div>
