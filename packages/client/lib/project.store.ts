e10034c5a1547a5b0d07df8cc87ff0ee9800e071
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-03 16:08:09 +0530

    ❇️ remove unused components

--- packages/client/lib/project.store.ts
+++ packages/client/lib/project.store.ts
@@ -1,57 +1,17 @@
 import create from 'zustand';
-import { Card, Project, Tab } from '../generated/graphql';
+import { Card } from '../generated/graphql';
 
 interface IProjectStateModel {
-  tabs: any[];
-  _id?: string;
-  projectDescription?: string;
-  projectName?: string;
-  setProject: (payload: Project) => void;
-  addTab: (payload: Tab) => void;
-  deleteTab: (id: string) => void;
+  cards: Card[];
   reset: () => void;
-  setCard: (tabId: string, data: Card[]) => void;
+  setCard: (data: Card[]) => void;
 }
 
 export const useProjectStore = create<IProjectStateModel>((set) => ({
-  projectDescription: '',
-  projectName: '',
-  tabs: [],
-  _id: '',
-  setProject: (data) =>
-    set(() => ({
-      _id: data._id,
-      projectDescription: data.projectDescription,
-      projectName: data.projectName,
-      tabs: data.tabs,
-    })),
-  addTab: (data) => set((state) => ({ tabs: [...state.tabs, data] })),
-  deleteTab: (id) =>
-    set((state) => ({
-      tabs: state.tabs.filter((t) => t._id !== id),
-    })),
+  cards: [],
   reset: () =>
     set(() => ({
-      projectDescription: '',
-      projectName: '',
-      tabs: [],
-      _id: '',
+      cards: [],
     })),
-  setCard: (tabId, data) =>
-    set((state) => {
-      return {
-        tabs: state.tabs.map((t) => {
-          if (t._id === tabId) {
-            t.cards.push({
-              _id: 'dsa',
-              __typename: 'Card',
-              cardBody: 'bineet',
-            });
-
-            return t;
-          }
-          return t;
-        }),
-      };
-    }),
+  setCard: (data) => set(() => ({ cards: data })),
 }));
--- packages/client/pages/project/[id].tsx
+++ packages/client/pages/project/[id].tsx
@@ -8,35 +8,26 @@ import {
 import { withApollo } from '../../lib/withApollo';
 import { useProjectStore } from '../../lib/project.store';
 import { useEffect } from 'react';
-import Tab from '../../components/Tab';
 
 const Project = () => {
   const router = useRouter();
-
   const { data, loading } = useGetProjectByIdQuery({
     variables: { id: router.query.id as string },
   });
-
-  const { setProject, projectName, tabs, reset } = useProjectStore();
-
+  const { reset, setCard } = useProjectStore();
   const [deleteProject] = useDeleteProjectMutation();
 
-  if (loading) return <Spinner />;
-
-  if (!data && !loading) return <h1>Somthing went wrong</h1>;
-
   useEffect(() => {
-    const unsub = setProject({
-      __typename: 'Project',
-      _id: data.getProjectById._id,
-      projectName: data.getProjectById.projectName,
-      projectDescription: data.getProjectById.projectDescription,
-      tabs: data.getProjectById.tabs,
-    });
-    return () => {
-      unsub;
-    };
+    if (data) {
+      const unsub = setCard(data.getProjectById.cards);
+      return () => {
+        unsub;
+      };
+    }
   }, [loading]);
+
+  if (loading) return <Spinner />;
+  if (!data && !loading) return <h1>Somthing went wrong</h1>;
 
   const handleDeleteProject = async () => {
     const id = data.getProjectById._id;
@@ -51,7 +42,9 @@ const Project = () => {
 
       <div className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4 w-4/6 m-auto">
         <header className="flex">
-          <h1 className="text-4xl font-bold uppercase flex-1">{projectName}</h1>
+          <h1 className="text-4xl font-bold uppercase flex-1">
+            {data.getProjectById.projectName}
+          </h1>
 
           <div>
             <button
@@ -68,12 +61,7 @@ const Project = () => {
 
         <hr />
 
-        <section className="grid grid-cols-4">
-          {tabs.map((t) => (
-            <Tab key={t._id} tab={t} />
-          ))}
-          <Tab addTab />
-        </section>
+        <section className="grid grid-cols-4"></section>
       </div>
     </div>
   );
