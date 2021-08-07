ed3215c0bf1c25dd02770f0c82799f332cd2716f
bineetNaidu <bineet.s.naidu@gmail.com>
2021-08-07 21:16:52 +0530

    ❇️ update user Models

--- packages/server/src/models/User.ts
+++ packages/server/src/models/User.ts
@@ -1,26 +1,28 @@
 import mongoose from 'mongoose';
-import { StringAndRequiredAndUnique } from './utils';
+import { StringAndRequired, StringAndRequiredAndUnique } from './utils';
 import Project from './Project';
+import bcrypt from 'bcrypt';
 
 export interface UserDoc extends mongoose.Document {
   username: string;
   avatar: string;
-  githubId: string;
+  password: string;
   projects: string[];
+  comparePassword: (password: string) => Promise<boolean>;
 }
 
 interface UserModel extends mongoose.Model<UserDoc> {
-  build(data: { username: string; avatar: string; githubId: string }): UserDoc;
+  build(data: { username: string; avatar: string; password: string }): UserDoc;
 }
 
 const UserSchema = new mongoose.Schema<UserDoc, UserModel>(
   {
     username: StringAndRequiredAndUnique,
+    password: StringAndRequired,
     avatar: {
       type: String,
       default: '',
     },
-    githubId: StringAndRequiredAndUnique,
     projects: [
       {
         type: mongoose.Schema.Types.ObjectId,
@@ -41,10 +43,20 @@ UserSchema.pre('remove', async function () {
   });
 });
 
+UserSchema.pre('save', async function () {
+  if (this.isNew) {
+    this.password = await bcrypt.hash(this.password, 14);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
+
 UserSchema.statics.build = (data: {
   username: string;
   avatar: string;
-  githubId: string;
+  password: string;
 }) => {
   return new User(data);
 };
